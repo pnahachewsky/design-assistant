6fe0f60656c0029608406da485dbec241c9cf514
Amber <amber.leblanc@cra-arc.gc.ca>
2025-12-23 12:01:14 -0500

    Change cra-design to proto-cra

---
+++ src/app/views/ia-assistant/services/validate-urls.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidateUrlsService {
+  //This might not need to exist as a service. Maybe all functions belong in component.
+
+}
