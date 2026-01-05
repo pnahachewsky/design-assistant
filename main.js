e67f39728c3fc16642f85d8ea22b5741b4ba1e0a
Parissa Nahachewsky <parissa.nahachewsky@cra-arc.gc.ca>
2026-01-05 09:34:40 -0800

    Auto-generated commit

--- main.js
+++ main.js
@@ -44702,7 +44702,7 @@ var routes = [
       }
       return true;
     }],
-    loadComponent: () => import("./chunk-O46UISBJ.js").then((m) => m.PageAssistantCompareComponent)
+    loadComponent: () => import("./chunk-NSVOPA4L.js").then((m) => m.PageAssistantCompareComponent)
   },
   {
     path: "page-assistant/share",
